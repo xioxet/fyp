cc9d269a6365fe6362bac99c01a498745c10ab0d
xioxet <qqqdyz@gmail.com>
2024-12-11 12:54:45 +0800

    CSRF DEMONS DEFEATED

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -1,12 +1,47 @@
-//import postgres from 'postgres';
-//
-//export const handle = async ({event, resolve}) => {
-//    const sql = postgres('postgres://postgres:postgres@fyp-postgres:5432/main');
-//
-//    event.locals = {
-//        sql: sql
-//    };
-//
-//    const response = await resolve(event);
-//    return response;
-//}
+/**
+ * CSRF protection copied from sveltekit but with the ability to turn it off for specific routes.
+ * Logic duplicated from `src/runtime/respond#respond` as of commit
+ * `008056b6ef33b554f8b03131c2635cc14b677ff1`
+ */
+export function csrf(allowedPaths) {
+    return async ({ event, resolve }) => {
+      const { request, url } = event;
+      const forbidden =
+        isFormContentType(request) &&
+        (request.method === "POST" ||
+          request.method === "PUT" ||
+          request.method === "PATCH" ||
+          request.method === "DELETE") &&
+        request.headers.get("origin") !== url.origin &&
+        !allowedPaths.includes(url.pathname);
+  
+      if (forbidden) {
+        const message = `Cross-site ${request.method} form submissions are forbidden`;
+        if (request.headers.get("accept") === "application/json") {
+          return json({ message }, { status: 403 });
+        }
+        return text(message, { status: 403 });
+      }
+  
+      return resolve(event);
+    };
+  }
+  
+  function isContentType(request, ...types) {
+    const type = request.headers.get("content-type")?.split(";", 1)[0].trim() ?? "";
+    return types.includes(type.toLowerCase());
+  }
+
+  function isFormContentType(request) {
+    return isContentType(
+      request,
+      "application/x-www-form-urlencoded",
+      "multipart/form-data",
+      "text/plain",
+    );
+  }
+
+export async function handle({event, resolve}) {
+    const CSRF = csrf(['/chat'])
+    return CSRF({event, resolve})    
+}
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,7 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    csrf: {
+      checkOrigin: false
+    } 
   },
   preprocess: vitePreprocess()
 };
