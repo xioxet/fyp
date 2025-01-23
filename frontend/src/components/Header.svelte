50e983adb33a6a72fe4a1ceecbecaa64e37ccb89
xioxet <qqqdyz@gmail.com>
2025-01-23 12:48:12 +0800

    end of an era...

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -1,6 +1,6 @@
 <div>
     <div class="w-90 px-10 h-10border-b-2 flex items-center bg-slate-900">
-        <div class="bg-slate-100 w-25 py-1 px-5 rounded-sm bg-slate-800 p-3 m-2 text-2xl mb-1.5">kita</div>
+        <div class="bg-slate-100 w-25 py-1 px-5 rounded-sm bg-slate-800 p-3 m-2 text-2xl mb-1.5">ai-cnc-app</div>
         <div class=grow></div>
         <a class="hover:bg-orange-500 rounded-sm bg-slate-800 p-3 m-2" href='/login'>login</a>
         <a class="hover:bg-orange-500 rounded-sm bg-slate-800 p-3 m-2" href='/upload'>upload</a>
